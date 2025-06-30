d5a8456fed12df403f85fcd9dc1f96f29ceda3c4
Silveratis0922 <tristan0922@hotmail.fr>
2025-06-30 20:03:32 +0200

    Probleme Java enfin regler !

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,27 @@
 FROM python:3.11-slim
 
-WORKDIR /app
 
-RUN apt-get update && apt-get install -y \
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    wget \
+    curl \
+    ca-certificates \
     gcc \
-    && rm -rf /var/lib/apt/lists/*
+    unzip && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN mkdir -p /opt/java && \
+    curl -L -o /opt/java/openjdk.tar.gz https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.20.1%2B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.20.1_1.tar.gz && \
+    tar -xzf /opt/java/openjdk.tar.gz -C /opt/java && \
+    mv /opt/java/jdk-11.0.20.1+1 /opt/java/java-11-openjdk && \
+    rm /opt/java/openjdk.tar.gz
+
+
+ENV JAVA_HOME=/opt/java/java-11-openjdk
+ENV PATH=$PATH:$JAVA_HOME/bin
+
+WORKDIR /app
 
 COPY requirements.txt .
 
---
+++ src/etl.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+
+
+def create_spark_session():
+    return SparkSession.builder \
+        .appName("CVReader") \
+        .getOrCreate()
+
+
+def read_csv(spark, file_path):
+    print(f"lecture du fichier: ", {file_path})
+
+    df = spark.read \
+        .option("header", "true") \
+        .option("inferShema", "true") \
+        .csv(file_path)
+
+    print(f"Nombre de lignes :", {df.count()})
+    print(f"Colonnes : {df.columns}")
+
+    print("Apercu : \n")
+    df.show(5)
+
+    print("Types des colonnes : \n")
+    df.printShema()
+
+    return df
+
+def main():
+    spark = create_spark_session()
+
+    try:
+        df = read_csv(spark, "../data/raw/data_2022_nov.csv")
+        print("Lecture reussie !")
+    except _ as e:
+        print ("Erreur.")
+
+if __name__ == "__main__":
+    main()
